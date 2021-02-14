a721bf55e81025a732f81c945bdd082e9cddf0f7
Yashwanth R <yash.rk@outlook.com>
2021-02-14 15:20:06 +0530

    Update dlsch_rx_stbc.c

--- dlsch_rx_stbc.c
+++ dlsch_rx_stbc.c
@@ -90,9 +90,6 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
 
   chPwr = ch11p + ch12p + ch21p + ch22p;
 
-  // LOG_UI(PHY, "Ch Pwr: %d\n", chPwr);
-
-  // HOW TO IMPLEMENT THIS USING INTEGERS
   a[0] = c[0] = 7071;
   b[0] = -2909; b[1] = 6444;
   d[0] = b[1]; d[1] = -b[0];
@@ -107,8 +104,6 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
   for (rb=0; rb<nb_rb; rb++) {
     for (re=0; re<((pilots==0)?12:8); re+=2) { 
 
-      // fprintf(p, "Symbol: %d | jj: %d | NRB: %d | re: %d | pilot: %d | ind: %d\n", symbol, (symbol*frame_parms->N_RB_DL*12), rb, re, pilots, jj);
-      jj += 8;
       if (Qm == 2) {
         QAM_TABLE = QAM_2;
       } else if (Qm == 4) {
@@ -117,100 +112,102 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
         QAM_TABLE = QAM_6;
       }
 
-      // Bruteforce Search over QAM constellations for two symbols
+      // Iterating over all possible value pairs of s3 and s4,
+      // we calculate s1 and s2 using closed form equations and then
+      // selec the pair (s1, s2, s3, s4) with minimum loss or squared error
       for (iter1 = 0; iter1 < (1 << Qm); iter1++) {
         for (iter2 = 0; iter2 < (1 << Qm); iter2++) {
           //  Compute Squared Error
 
+          // Assuming the s3 and s4 is known by iterating over every possible value
           s3[0] = QAM_TABLE[2 * iter1];
           s3[1] = QAM_TABLE[2 * iter1 + 1];
           s4[0] = QAM_TABLE[2 * iter2];
           s4[1] = QAM_TABLE[2 * iter2 + 1];
 
-          // LOG_UI(PHY, "s3: %d %d\n", s3[0], s3[1]);
-          
-          // LOG_UI(PHY, "s4: %d %d\n", s4[0], s4[1]);
-
-          // LOG_UI(PHY, "ch_11: %d %d\n", ch_11[0], ch_11[1]);
-          
-          // LOG_UI(PHY, "ch_12: %d %d\n", ch_12[0], ch_12[1]);
-
-          // z11 = dlsch_stbc_mul(ch_11, s3, 0, 0);
+         // Calculating Equations (7a, 7b, 7c, 7d) from Paper
+         
+         // Calculating s3 * h_11
           z11[0] = (ch_11[0] * s3[0] - ch_11[1] * s3[1]) / symAmp;
           z11[1] = (ch_11[0] * s3[1] + ch_11[1] * s3[0]) / symAmp;
 
-          // LOG_UI(PHY, "z11: %d %d\n", z11[0], z11[1]);
-          
-          // z12 = dlsch_stbc_mul(ch_12, s4, 0, 0);
+         // Calculating s4 * h_12
           z12[0] = (ch_12[0] * s4[0] - ch_12[1] * s4[1]) / symAmp;
           z12[1] = (ch_12[0] * s4[1] + ch_12[1] * s4[0]) / symAmp;
-          // LOG_UI(PHY, "z12: %d %d\n", z12[0], z12[1]);
 
-          // dlsch_stbc_add(z11, z12)
+         // Calculating (s3 * h_11 + s4 * h_12)
           z1[0] = z11[0] + z12[0];
           z1[1] = z11[1] + z12[1];
-          // LOG_UI(PHY, "z1: %d %d\n", z1[0], z1[1]);
 
-          //  dlsch_stbc_mul(b, z1, 0, 0)
+         // Calculating b * (s3 * h_11 + s4 * h_12)
           z12[0] = ((z1[0] * b[0]) - (z1[1] * b[1])) / symAmp;
           z12[1] = ((z1[0] * b[1]) + (z1[1] * b[0])) / symAmp;
-          // LOG_UI(PHY, "z12: %d %d\n", z12[0], z12[1]);
 
-          // z1 = dlsch_stbc_sub(rxF0, z12);
+         // Calculating r1 - (b * (s3 * h_11 + s4 * h_12))
           z1[0] = rxF0[0] - z12[0];
           z1[1] = rxF0[1] - z12[1];
 
-          // z21 = dlsch_stbc_mul(ch_12, s3, 0, 1);
+         // Calculating h_12 * conj(s3)
           z21[0] = (ch_12[0] * s3[0] - ch_12[1] * -s3[1]) / symAmp;
           z21[1] = (ch_12[0] * -s3[1] + ch_12[1] * s3[0]) / symAmp;
 
-          // z22 = dlsch_stbc_mul(ch_11, s4, 0, 1);
+         // Calculating h_11 * conj(s4)
           z22[0] = (ch_11[0] * s4[0] - ch_11[1] * -s4[1]) / symAmp;
           z22[1] = (ch_11[0] * -s4[1] + ch_11[1] * s4[0]) / symAmp;
+
+         // Calculating (h_12 * conj(s3) - h_11 * conj(s4))
           z21[0] = z21[0] - z22[0];
           z21[1] = z21[1] - z22[1];
 
-          // dlsch_stbc_mul(d, z21, 0, 0)
+         // Calculating d * (h_12 * conj(s3) - h_11 * conj(s4))
           z22[0] = ((z21[0] * d[0]) - (z21[1] * d[1])) / symAmp;
           z22[1] = ((z21[0] * d[1]) + (z21[1] * d[0])) / symAmp;
-          // z2 = dlsch_stbc_sub(rxF0 + 2, z22);
+
+         // Calculating r2 - (d * (h_12 * conj(s3) - h_11 * conj(s4)))
           z2[0] = rxF0[2] - z22[0];
           z2[1] = rxF0[3] - z22[1];
-          // z31 = dlsch_stbc_mul(ch_21, s3, 0, 0);
+          
+         // Calculating h_21 * s3
           z31[0] = (ch_21[0] * s3[0] - ch_21[1] * s3[1]) / symAmp;
           z31[1] = (ch_21[0] * s3[1] + ch_21[1] * s3[0]) / symAmp;
-          // z32 = dlsch_stbc_mul(ch_22, s4, 0, 0);
+
+         // Calculating h_22 * s4
           z32[0] = (ch_22[0] * s4[0] - ch_22[1] * s4[1]) / symAmp;
           z32[1] = (ch_22[0] * s4[1] + ch_22[1] * s4[0]) / symAmp;
 
+         // Calculating (h_21 * s3) + (h_22 * s4)
           z31[0] = z31[0] + z32[0];
           z31[1] = z31[1] + z32[1];
 
+         // Calculating b * ((h_21 * s3) + (h_22 * s4))
           z32[0] = ((z31[0] * b[0]) - (z31[1] * b[1])) / symAmp;
           z32[1] = ((z31[0] * b[1]) + (z31[1] * b[0])) / symAmp;      
 
+         // Calculating r3 - (b * ((h_21 * s3) + (h_22 * s4)))
           z3[0] = rxF1[0] - z32[0];
           z3[1] = rxF1[1] - z32[1];
 
+         // Calculating h_22 * conj(s3)
           z41[0] = (ch_22[0] * s3[0] - ch_22[1] * -s3[1]) / symAmp;
           z41[1] = (ch_22[0] * -s3[1] + ch_22[1] * s3[0]) / symAmp;
           
-          // z42 = dlsch_stbc_mul(ch_21, s4, 0, 1);
+         // Calculating h_21 * conj(s4)
           z42[0] = (ch_21[0] * s4[0] - ch_21[1] * -s4[1]) / symAmp;
           z42[1] = (ch_21[0] * -s4[1] + ch_21[1] * s4[0]) / symAmp;
           
+         // Calculating (h_22 * conj(s3) - h_21 * conj(s4))
           z41[0] = z41[0] - z42[0];
           z41[1] = z41[1] - z42[1];
           
-          // dlsch_stbc_mul(d, z41, 0, 0)
+         // Calculating d * (h_22 * conj(s3) - h_21 * conj(s4))
           z42[0] = ((z41[0] * d[0]) - (z41[1] * d[1])) / symAmp;
           z42[1] = ((z41[0] * d[1]) + (z41[1] * d[0])) / symAmp;    
           
-          // z4 = dlsch_stbc_sub(rxF1 + 2, z42);
+         // Calculating r4 - d * (h_22 * conj(s3) - h_21 * conj(s4))
           z4[0] = rxF1[2] - z42[0];
           z4[1] = rxF1[3] - z42[1];
           
-          // s1 = dlsch_stbc_add(dlsch_stbc_div(dlsch_stbc_add(dlsch_stbc_mul(ch_11, z1, 1, 0), dlsch_stbc_mul(ch_21, z3, 1, 0)), a), dlsch_stbc_div(dlsch_stbc_add(dlsch_stbc_mul(ch_12, z2, 0, 1), dlsch_stbc_mul(ch_22, z4, 0, 1)), c));
+         // Calculating s1 based on equation (10)
           s1[0] = (ch_11[0] * z1[0] - (-ch_11[1]) * z1[1]);
           s1[1] = (ch_11[0] * z1[1] + (-ch_11[1]) * z1[0]);
           
@@ -229,7 +226,7 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
           s1[0] = (int32_t)((int64_t)s1[0] * symAmp / chPwr);
           s1[1] = (int32_t)((int64_t)s1[1] * symAmp / chPwr);
 
-          // s2 = dlsch_stbc_sub(dlsch_stbc_div(dlsch_stbc_add(dlsch_stbc_mul(ch_12, z1, 1, 0), dlsch_stbc_mul(ch_22, z3, 1, 0)), a), dlsch_stbc_div(dlsch_stbc_add(dlsch_stbc_mul(ch_11, z2, 0, 1), dlsch_stbc_mul(ch_21, z4, 0, 1)), c));
+         // Calculating s2 based on equation (11)
           s2[0] = (ch_12[0] * z1[0] - (-ch_12[1]) * z1[1]);
           s2[1] = (ch_12[0] * z1[1] + (-ch_12[1]) * z1[0]);
 
@@ -248,148 +245,160 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
           s2[0] = (int32_t)((int64_t)s2[0] * symAmp / chPwr);
           s2[1] = (int32_t)((int64_t)s2[1] * symAmp / chPwr);
 
-          // z11 = dlsch_stbc_mul(a, s1, 0, 0);
+          // Now, for a certain pair of (s3, s4) - we calculated (s1, s2)
+          // and will calculate the squared error with respect to the received
+          // signal and channel conditions. The pair (s1, s2, s3, s4) which
+          // results in least error is chosen as the received value given by
+          // the definiton of "D(s1, s2, s3, s4)" in the paper on page 2.
+
+          // Calculating s1 * a
           z11[0] = (a[0] * s1[0] - a[1] * s1[1]) / symAmp;
           z11[1] = (a[0] * s1[1] + a[1] * s1[0]) / symAmp;
 
-          // z11_tmp = b * s3
+          // Calculating b * s3
           z11_tmp[0] = (b[0] * s3[0] - b[1] * s3[1]) / symAmp;
           z11_tmp[1] = (b[0] * s3[1] + b[1] * s3[0]) / symAmp;
 
+         // Calculating a * s1 + b * s3
           z11[0] = (z11[0] + z11_tmp[0]);
           z11[1] = (z11[1] + z11_tmp[1]);
 
-          //  z11 = z11 * ch_11
+          // Calculating h_11 * (a * s1 + b * s3)
           z11_tmp[0] = (ch_11[0] * z11[0] - ch_11[1] * z11[1]);
           z11_tmp[1] = (ch_11[0] * z11[1] + ch_11[1] * z11[0]);
 
-          // z11 = dlsch_stbc_mul(a, s1, 0, 0);
+          // Calculating a * s2
           z12[0] = (a[0] * s2[0] - a[1] * s2[1]) / symAmp;
           z12[1] = (a[0] * s2[1] + a[1] * s2[0]) / symAmp;
 
-          // z12_tmp = b * s3
+          // Calculating b * s4
           z12_tmp[0] = (b[0] * s4[0] - b[1] * s4[1]) / symAmp;
           z12_tmp[1] = (b[0] * s4[1] + b[1] * s4[0]) / symAmp;
 
+         // Calculating (a * s2 + b * s4)
           z12[0] = (z12[0] + z12_tmp[0]);
           z12[1] = (z12[1] + z12_tmp[1]);
 
-          //  z12 = z12 * ch_12
+         // Calculating h_12 * (a * s2 + b * s4)
           z12_tmp[0] = (ch_12[0] * z12[0] - ch_12[1] * z12[1]);
           z12_tmp[1] = (ch_12[0] * z12[1] + ch_12[1] * z12[0]);
 
+         // Calculating z1 = h_12 * (a * s2 + b * s4) + h_11 * (a * s1 + b * s3)
           z1[0] = z11_tmp[0] + z12_tmp[0];
           z1[1] = z11_tmp[1] + z12_tmp[1];
      
   // --------------------------------------------------------------------------------------------
+         // Calculating c * conj(s1)
           z22[0] = (c[0] * s1[0] - c[1] * -s1[1]) / symAmp;
           z22[1] = (c[0] * -s1[1] + c[1] * s1[0]) / symAmp;
-          // LOG_UI(PHY, "z22: %d %d\n", z22[0], z22[1]);
 
-          // z22_tmp = b * s3
+         // Calculating d * conj(s3)
           z22_tmp[0] = (d[0] * s3[0] - d[1] * -s3[1]) / symAmp;
           z22_tmp[1] = (d[0] * -s3[1] + d[1] * s3[0]) / symAmp;
-          // LOG_UI(PHY, "z22_tmp: %d %d\n", z22_tmp[0], z22_tmp[1]);
 
+         // Calculating (c * conj(s1) + d * conj(s3))
           z22[0] = (z22[0] + z22_tmp[0]);
           z22[1] = (z22[1] + z22_tmp[1]);
-          // LOG_UI(PHY, "z22: %d %d\n", z22[0], z22[1]);
 
-          //  z11 = z11 * ch_11
+         // Calculating h_12 * (c * conj(s1) + d * conj(s3))
           z22_tmp[0] = (ch_12[0] * z22[0] - ch_12[1] * z22[1]);
           z22_tmp[1] = (ch_12[0] * z22[1] + ch_12[1] * z22[0]);
-          // LOG_UI(PHY, "z22_tmp: %d %d\n", z22_tmp[0], z22_tmp[1]);
-  
 
+         // Calculating d * conj(s4)
           z21_tmp[0] = (d[0] * s4[0] - d[1] * -s4[1]) / symAmp;
           z21_tmp[1] = (d[0] * -s4[1] + d[1] * s4[0]) / symAmp;
-          // LOG_UI(PHY, "z21_tmp: %d %d\n", z21_tmp[0], z21_tmp[1]);
 
+         // Calculating c * conj(s2)
           z21[0] = (c[0] * s2[0] - c[1] * -s2[1]) / symAmp;
           z21[1] = (c[0] * -s2[1] + c[1] * s2[0]) / symAmp;
-          // LOG_UI(PHY, "z21: %d %d\n", z21[0], z21[1]);
 
+         // Calculating (c * conj(s2) + d * conj(s3))
           z21[0] = (z21[0] + z21_tmp[0]);
           z21[1] = (z21[1] + z21_tmp[1]);
-          // LOG_UI(PHY, "z21: %d %d\n", z21[0], z21[1]);
 
-          //  z11 = z11 * ch_11
+         // Calculating h_11 * (c * conj(s2) + d * conj(s3))
           z21_tmp[0] = (ch_11[0] * z21[0] - ch_11[1] * z21[1]);
           z21_tmp[1] = (ch_11[0] * z21[1] + ch_11[1] * z21[0]);
-          // LOG_UI(PHY, "z21_tmp: %d %d\n", z21_tmp[0], z21_tmp[1]);
 
-          // LOG_UI(PHY, "z21_tmp: %d %d\n", z21_tmp[0], z21_tmp[1]);
-          // LOG_UI(PHY, "z22_tmp: %d %d\n", z22_tmp[0], z22_tmp[1]);
+         // Calculating z2 = h_12 * (c * conj(s1) + d * conj(s3)) - h_11 * (c * conj(s2) + d * conj(s3))
           z2[0] = z22_tmp[0] - z21_tmp[0];
           z2[1] = z22_tmp[1] - z21_tmp[1];
-          // LOG_UI(PHY, "z2: %d %d\n", z2[0], z2[1]);
-          // exit(0);
+
 // -------------------------------------------  -------------------------
+         // Calculating a * s1
           z31[0] = (a[0] * s1[0] - a[1] * s1[1]) / symAmp;
           z31[1] = (a[0] * s1[1] + a[1] * s1[0]) / symAmp;
 
-          // z31_tmp = b dlsch_rx_stbc* s3
+         // Calculating b * s3
           z31_tmp[0] = (b[0] * s3[0] - b[1] * s3[1]) / symAmp;
           z31_tmp[1] = (b[0] * s3[1] + b[1] * s3[0]) / symAmp;
 
+         // Calculating a * s1 + b * s3
           z31[0] = (z31[0] + z31_tmp[0]);
           z31[1] = (z31[1] + z31_tmp[1]);
 
-          //  z31 = z31 * ch_11
+         // Calculating h_21 * (a * s1 + b * s3)
           z31_tmp[0] = (ch_21[0] * z31[0] - ch_21[1] * z31[1]);
           z31_tmp[1] = (ch_21[0] * z31[1] + ch_21[1] * z31[0]);
 
-          // z31 = dlsch_stbc_mul(a, s1, 0, 0);
+         // Calculating a * s2
           z32[0] = (a[0] * s2[0] - a[1] * s2[1]) / symAmp;
           z32[1] = (a[0] * s2[1] + a[1] * s2[0]) / symAmp;
 
-          // z32_tmp = b * s3
+         // Calculating b * s4
           z32_tmp[0] = (b[0] * s4[0] - b[1] * s4[1]) / symAmp;
           z32_tmp[1] = (b[0] * s4[1] + b[1] * s4[0]) / symAmp;
 
+         // Calculating a * s2 + b * s4
           z32[0] = (z32[0] + z32_tmp[0]);
           z32[1] = (z32[1] + z32_tmp[1]);
 
-          //  z32 = z32 * ch_12
+         // Calculating h_22 * (a * s2 + b * s4)
           z32_tmp[0] = (ch_22[0] * z32[0] - ch_22[1] * z32[1]);
           z32_tmp[1] = (ch_22[0] * z32[1] + ch_22[1] * z32[0]);
 
+         // Calculating z3 = h_22 * (a * s2 + b * s4) + h_21 * (a * s1 + b * s3)
           z3[0] = z31_tmp[0] + z32_tmp[0];
           z3[1] = z31_tmp[1] + z32_tmp[1];
 
   // --------------------------------------------------------------------------------------------
+         // Calculating c * conj(s1)
           z42[0] = (c[0] * s1[0] - c[1] * -s1[1]) / symAmp;
           z42[1] = (c[0] * -s1[1] + c[1] * s1[0]) / symAmp;
 
-          // z42_tmp = b * s3
+         // Calculating d * conj(s3)
           z42_tmp[0] = (d[0] * s3[0] - d[1] * -s3[1]) / symAmp;
           z42_tmp[1] = (d[0] * -s3[1] + d[1] * s3[0]) / symAmp;
 
+         // Calculating (c * conj(s1) + d * conj(s3))
           z42[0] = (z42[0] + z42_tmp[0]);
           z42[1] = (z42[1] + z42_tmp[1]);
 
-          //  z11 = z11 * ch_11
+         // Calculating h_22 * (c * conj(s1) + d * conj(s3))
           z42_tmp[0] = (ch_22[0] * z42[0] - ch_22[1] * z42[1]);
           z42_tmp[1] = (ch_22[0] * z42[1] + ch_22[1] * z42[0]);
 
+         // Calculating c * conj(s2)
           z41[0] = (c[0] * s2[0] - c[1] * -s2[1]) / symAmp;
           z41[1] = (c[0] * -s2[1] + c[1] * s2[0]) / symAmp;
 
-          // z41_tmp = b * s3
+         // Calculating d * conj(s4)
           z41_tmp[0] = (d[0] * s4[0] - d[1] * -s4[1]) / symAmp;
           z41_tmp[1] = (d[0] * -s4[1] + d[1] * s4[0]) / symAmp;
 
+         // Calculating c * conj(s3) + d * conj(s4)
           z41[0] = (z41[0] + z41_tmp[0]);
           z41[1] = (z41[1] + z41_tmp[1]);
 
-          //  z11 = z11 * ch_11
+         // Calculating h_21 * (c * conj(s3) + d * conj(s4))
           z41_tmp[0] = (ch_21[0] * z41[0] - ch_21[1] * z41[1]);
           z41_tmp[1] = (ch_21[0] * z41[1] + ch_21[1] * z41[0]);
 
+         // Calculating z4 = h_22 * (c * conj(s1) + d * conj(s3)) - h_21 * (c * conj(s3) + d * conj(s4))
           z4[0] = z42_tmp[0] - z41_tmp[0];
           z4[1] = z42_tmp[1] - z41_tmp[1];
 
-
+          // Amplitude scaling for fixed point implementation
           z1[0] = z1[0] / symAmp;
           z1[1] = z1[1] / symAmp;
           z2[0] = z2[0] / symAmp;
@@ -400,6 +409,8 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
           z4[1] = z4[1] / symAmp;
           
 
+        // Calculating |Received signal - Channel * txSignal_hat| ^ 2
+        // (I have reduced the amplitude of the error by 2^Qm to avoid overflow. This doesn't change the output)
           MSE[iter1][iter2] += (uint64_t)(((int32_t)(rxF0[0]) - z1[0]) >> (Qm)) * (uint64_t)(((int32_t)(rxF0[0]) - z1[0]) >> (Qm));
           MSE[iter1][iter2] += (uint64_t)(((int32_t)(rxF0[1]) - z1[1]) >> (Qm)) * (uint64_t)(((int32_t)(rxF0[1]) - z1[1]) >> (Qm));
           MSE[iter1][iter2] += (uint64_t)(((int32_t)(rxF0[2]) - z2[0]) >> (Qm)) * (uint64_t)(((int32_t)(rxF0[2]) - z2[0]) >> (Qm));
@@ -411,14 +422,12 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
         }
       }
 
-      // Find the(iter_qam1, iter_qam2) combination with least squared error
+      // Find the (s3, s4) pair with least squared error
       min = MSE[0][0];
 
       for (iter1 = 0; iter1 < (1 << Qm); iter1++) {
         for (iter2 = 0; iter2 < (1 << Qm); iter2++) {
-          if (rb == 3) {
           LOG_UI(PHY, "%d ", MSE[iter1][iter2]);
-          }
           if (MSE[iter1][iter2] < min) {
               min = MSE[iter1][iter2];
               x = iter1;
@@ -426,22 +435,21 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
           }
           MSE[iter1][iter2] = 0;
         }
-        if (rb == 3) {
-          LOG_UI(PHY, "\n");
-        }
+        LOG_UI(PHY, "\n");
       }
 
       qam_pt[0] = x;
       qam_pt[1] = y;
 
       // LOG_UI(PHY, "min QAM x: %d - y: %d\n", x, y);
-      // Rest two symbols have a closed form solutions given that first two symbols are known
 
+      // (s3, s4) with least error
       s3[0] = QAM_TABLE[qam_pt[0] * 2];
       s3[1] = QAM_TABLE[qam_pt[0] * 2 + 1];
       s4[0] = QAM_TABLE[qam_pt[1] * 2];
       s4[1] = QAM_TABLE[qam_pt[1] * 2 + 1];
 
+     // Calculating (s1, s2) as done previously but only for the (s3, s4) pair with least error
       // z11 = dlsch_stbc_mul(ch_11, s3, 0, 0);
       z11[0] = (ch_11[0] * s3[0] - ch_11[1] * s3[1]) / symAmp;
       z11[1] = (ch_11[0] * s3[1] + ch_11[1] * s3[0]) / symAmp;
@@ -462,7 +470,7 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
       // z1 = dlsch_stbc_sub(rxF0, z12);
       z1[0] = rxF0[0] - z12[0];
       z1[1] = rxF0[1] - z12[1];
-      // exit(0);
+
       // z21 = dlsch_stbc_mul(ch_12, s3, 0, 1);
       z21[0] = (ch_12[0] * s3[0] - ch_12[1] * -s3[1]) / symAmp;
       z21[1] = (ch_12[0] * -s3[1] + ch_12[1] * s3[0]) / symAmp;
@@ -563,30 +571,6 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
       s4[1] = s4[1] * 180 / 7071;
 
 
-      if (rb == 3) {
-
-        LOG_UI(PHY, "rxF0: %d %d\n", rxF0[0], rxF0[1]);
-        LOG_UI(PHY, "rxF1: %d %d\n", rxF1[0], rxF1[1]);
-        LOG_UI(PHY, "rxF0: %d %d\n", rxF0[2], rxF0[3]);
-        LOG_UI(PHY, "rxF1: %d %d\n", rxF1[2], rxF1[3]);
-
-        LOG_UI(PHY, "ch_11: %d %d\n", ch_11[0], ch_11[1]);
-        LOG_UI(PHY, "ch_12: %d %d\n", ch_12[0], ch_12[1]);
-        LOG_UI(PHY, "ch_21: %d %d\n", ch_21[0], ch_21[1]);
-        LOG_UI(PHY, "ch_22: %d %d\n", ch_22[0], ch_22[1]);
-
-        LOG_UI(PHY, "s1: %d %d\n", s1[0], s1[1]);
-        LOG_UI(PHY, "s2: %d %d\n", s2[0], s2[1]);
-        LOG_UI(PHY, "s3: %d %d\n", s3[0], s3[1]);
-        LOG_UI(PHY, "s4: %d %d\n", s4[0], s4[1]);
-
-        exit(0);
-
-      }
-
-      
-      // exit(0);
-
       rxF0_comp[0] = (int16_t)s1[0];
       rxF0_comp[1] = (int16_t)s1[1];
       rxF0_comp[2] = (int16_t)s2[0];
@@ -602,7 +586,6 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
       fprintf(p, "symbol: %d | offset: %d | NRB: %d | re: %d | %d %d\n", symbol, (symbol * 12 * 25), rb, re+1, rxF0_comp[6], rxF0_comp[7]);
 
       rxF0_comp += 8;
-      indSTBC += 8;
       rxF0 += 4;
       rxF1 += 4;
       ch_11 += 2;
@@ -614,9 +597,6 @@ void dlsch_rx_stbc(LTE_DL_FRAME_PARMS *frame_parms,
   }
   fclose(p);
 
-  if (symbol == 6) {
-    indSTBC = 0;
-  }
 
   free(s1);
   free(s2);
